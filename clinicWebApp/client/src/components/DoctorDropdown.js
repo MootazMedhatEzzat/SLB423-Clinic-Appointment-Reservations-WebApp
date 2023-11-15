12055499d2984f30b64c2938374b68021e0b7c7c
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-11-15 19:29:58 +0200

    Create DoctorDropdown.js

---
+++ clinicWebApp/client/src/components/DoctorDropdown.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function DoctorDropdown({ onSelect }) {
+  const [doctors, setDoctors] = useState([]);
+
+  useEffect(() => {
+    async function fetchDoctors() {
+      const response = await axios.get('/api/patients/getdoctors');
+      setDoctors(response.data.doctors);
+    }
+    fetchDoctors();
+  }, []);
+
+  function handleSelect(event) {
+    const doctorId = event.target.value;
+    onSelect(doctorId);
+  }
+
+  return (
+    <select onChange={handleSelect}>
+      <option value="">Select a doctor</option>
+      {doctors.map(doctor => (
+        <option key={doctor.doctor_id} value={doctor.doctor_id}>
+          Dr.{doctor.name}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+export default DoctorDropdown;
