16162fd31b2e9559917dd2aa2f0aa6614759cac0
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-11-15 19:49:35 +0200

    Update doctors.js

--- clinicWebApp/server/src/routes/doctors.js
+++ clinicWebApp/server/src/routes/doctors.js
@@ -5,7 +5,8 @@ const router = express.Router();
 const doctorController = require('../controllers/doctorController');
 
 // Doctor routes
-router.post('/doctors/:id/addslot', doctorController.addDoctorSlot);
-router.delete('/doctors/:id/slots/:slotId/cancelslot', doctorController.cancelDoctorSlot);
+router.post('/doctors/addslot', doctorController.addDoctorSlot);
+router.delete('/doctors/cancelslot', doctorController.cancelDoctorSlot);
+router.get('/doctors/slots/:id', doctorController.doctorSlots);
 
 module.exports = router;
