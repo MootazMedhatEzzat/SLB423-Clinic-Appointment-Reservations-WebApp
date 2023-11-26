acee703830ce76ce663f9733bbba0df6c7207237
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-11-26 20:40:37 +0200

    Create server.js

---
+++ clinicWebApp/client/server.js
@@ -0,0 +1,17 @@
+// server.js
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(3001, () => {
+  console.log('Frontend server is running on port 3001');
+});
