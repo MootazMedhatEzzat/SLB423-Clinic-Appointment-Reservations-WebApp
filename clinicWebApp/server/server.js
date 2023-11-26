4a0a1087df3eea959c591b6347d0d66be036a6b4
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-11-26 20:30:22 +0200

    Update server.js

--- clinicWebApp/server/server.js
+++ clinicWebApp/server/server.js
@@ -3,15 +3,10 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.json());
-
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
 
 const doctorRoutes = require('./src/routes/doctors');
 const patientRoutes = require('./src/routes/patients');
@@ -22,11 +17,6 @@ app.use('/api', doctorRoutes);
 app.use('/api', patientRoutes);
 app.use('/api', authRoutes);
 
-// Serve the React app for any other requests
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
 app.listen(3000, () => {
-  console.log('Server Is Running on Port 3000');
+  console.log('Backend server Is Running on Port 3000');
 });
