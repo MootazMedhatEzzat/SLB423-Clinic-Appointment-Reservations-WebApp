57b5d890c72ca31347137a02107aa9f32fef33de
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-11-28 02:30:34 +0200

    Create deploy.sh
    Create bash script to include all the required commands to be run at once by running the bash script rather than trying them one by one

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Create docker networks
+docker network create ui-network
+docker network create api-network
+
+# Build and run database container
+cd clinicWebApp/database
+docker build -t clinic-web-database-image .
+docker run -d -p 5432:5432 --name clinic-web-database-container --network api-network -v clinic-web-data:/var/lib/postgresql/data clinic-web-database-image:latest
+
+# Build and run backend server container
+cd ../server
+docker build -t clinic-web-server .
+docker run -d -p 3000:3000 --name clinic-web-backend --network api-network clinic-web-server:latest
+
+# Connect backend server to ui-network
+docker network connect ui-network clinic-web-backend
+
+# Build and run frontend client container
+cd ../client
+docker build -t clinic-web-client .
+docker run -d -p 3001:3001 --name clinic-web-frontend --network ui-network clinic-web-client:latest
+
+# Return to the initial directory
+cd ../..
