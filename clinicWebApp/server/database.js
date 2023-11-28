e72f8058fd4ee962550ace9ecfce66b1bd046927
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-11-28 15:36:10 +0200

    Update database.js

--- clinicWebApp/server/database.js
+++ clinicWebApp/server/database.js
@@ -6,7 +6,7 @@ const pool = new Pool({
     user: 'postgres',
     host: 'clinic-web-database-container',
     database: 'clinic_appointments_reservations',
-    password: 'database_password', (In a development or local environment where security is not a major concern, we might be able to proceed without a password)
+    password: 'database_password',
     port: 5432,
 });
 
