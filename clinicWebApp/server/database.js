39bb077928f02d3ecc7d499b767ee85bb8cefad4
Salma Hossam elden Hassan Mohamed <88389052+SalmHossam@users.noreply.github.com>
2023-11-30 21:54:07 +0200

    Update database.js

--- clinicWebApp/server/database.js
+++ clinicWebApp/server/database.js
@@ -5,8 +5,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'postgres',
     host: 'clinic-web-database-container',
-    database: 'clinic_appointments_reservations',
-    password: '17March@2003',
+    database: 'clinic_database',
+    password: 'clinic_database_pass',
     port: 5432,
 });
 
