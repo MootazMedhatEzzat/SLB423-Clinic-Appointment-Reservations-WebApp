5972dcc2f719bf2b14f9891b6529b64bd2e841dc
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-12-15 21:08:59 +0200

    Update server.js

--- clinicWebApp/server/server.js
+++ clinicWebApp/server/server.js
@@ -3,8 +3,11 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
 const app = express();
+
+const defaultPort = 3000;
+const port = process.env.BE_PORT || process.argv[2] || defaultPort;
+
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -12,11 +15,10 @@ const doctorRoutes = require('./src/routes/doctors');
 const patientRoutes = require('./src/routes/patients');
 const authRoutes = require('./src/routes/auth');
 
-// API routes
 app.use('/api', doctorRoutes);
 app.use('/api', patientRoutes);
 app.use('/api', authRoutes);
 
-app.listen(3000, () => {
-  console.log('Backend server Is Running on Port 3000');
+app.listen(port, () => {
+  console.log(`Backend server is running on port ${port}`);
 });
