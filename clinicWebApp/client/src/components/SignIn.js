bb19bcb1ea665c8c56b209e2d38c745ea5e3eaa0
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-12-17 01:01:21 +0200

    Update SignIn.js

--- clinicWebApp/client/src/components/SignIn.js
+++ clinicWebApp/client/src/components/SignIn.js
@@ -17,7 +17,7 @@ const SignIn = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('https://clinic-web-server-mootazmwahab-dev.apps.sandbox-m3.1530.p1.openshiftapps.com/api/signin', user);
+      const response = await axios.post('/api/signin', user);
       localStorage.setItem('token', response.data.token);
       localStorage.setItem('userId', response.data.userId);
       localStorage.setItem('name', response.data.name); 
