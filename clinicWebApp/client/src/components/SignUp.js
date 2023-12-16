01aeba32753b2319dded2b747c081dc5a7c7280e
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-12-17 00:32:20 +0200

    Update SignUp.js

--- clinicWebApp/client/src/components/SignUp.js
+++ clinicWebApp/client/src/components/SignUp.js
@@ -20,7 +20,7 @@ const SignUp = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:3000/api/signup', user);
+      const response = await axios.post('http://clinic-web-server.mootazmwahab-dev.svc.cluster.local/api/signup', user);
       localStorage.setItem('token', response.data.token); 
       localStorage.setItem('userId', response.data.userId); 
       localStorage.setItem('name', user.name); 
