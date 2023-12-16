266dc392823b5a09f2353c27613d50f96c91495b
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-12-16 16:01:48 +0200

    Update SlotsTable.js

--- clinicWebApp/client/src/components/SlotsTable.js
+++ clinicWebApp/client/src/components/SlotsTable.js
@@ -1,34 +1,36 @@
 import React from 'react';
 
-// Function to format the date in a more readable format
 const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    return date.toLocaleDateString('en-US', options);
-};
-  
-  // Function to format the time in AM/PM format
-  const formatTime = (timeString) => {
-    const time = new Date(`2000-01-01T${timeString}`);
-    return time.toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit' });
+  const date = new Date(dateString);
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return date.toLocaleDateString('en-US', options);
 };
 
-function SlotsTable({ slots }) {
+const formatTime = (timeString) => {
+  const time = new Date(`2000-01-01T${timeString}`);
+  return time.toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit' });
+};
+
+function SlotsTable({ slots, onBookAppointment }) {
   return (
     <table>
       <thead>
         <tr>
-          <th>ID</th>
           <th>Appointment</th>
           <th>Reservations Number</th>
+          <th>Book Appointment</th>
         </tr>
       </thead>
       <tbody>
-        {slots.map(slot => (
+        {slots.map((slot) => (
           <tr key={slot.slot_id}>
-            <td>{slot.id}</td>
-            <td>{formatDate(slot.date)} | {formatTime(slot.start_time)} - {formatTime(slot.end_time)}</td>
-            <td>{slot.reservations_num}</td>
+            <td style={{ textAlign: 'center' }}>
+              {formatDate(slot.date)} | {formatTime(slot.start_time)} - {formatTime(slot.end_time)}
+            </td>
+            <td style={{ textAlign: 'center' }}>{slot.reservations_num}</td>
+            <td style={{ textAlign: 'center' }}>
+              <button onClick={() => onBookAppointment(slot.slot_id)}>Book</button>
+            </td>
           </tr>
         ))}
       </tbody>
