c27feef1ce83f89f60ecd10abf1fd9575ce313ea
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-12-17 01:34:58 +0200

    Update server.js

--- clinicWebApp/server/server.js
+++ clinicWebApp/server/server.js
@@ -9,7 +9,16 @@ const defaultPort = 3000;
 const port = process.env.BE_PORT || process.argv[2] || defaultPort;
 
 app.use(bodyParser.json());
-app.use(cors());
+//app.use(cors());
+
+const corsOptions = {
+  origin: 'https://clinic-web-client-mootazmwahab-dev.apps.sandbox-m3.1530.p1.openshiftapps.com',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 
 const doctorRoutes = require('./src/routes/doctors');
 const patientRoutes = require('./src/routes/patients');
