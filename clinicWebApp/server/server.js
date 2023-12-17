18ccf05bc3eb8bd85ddea6601a5b0ea5eada003a
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-12-17 16:01:20 +0200

    Update server.js

--- clinicWebApp/server/server.js
+++ clinicWebApp/server/server.js
@@ -13,22 +13,11 @@ app.use(bodyParser.json());
 // Configure CORS to allow requests from a specific origin (e.g., https://example.com)
 const corsOptions = {
   origin: 'https://clinic-web-client-mootazmwahab-dev.apps.sandbox-m3.1530.p1.openshiftapps.com',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
 
 //app.use(cors());
-
-// Handle preflight requests explicitly for the root path
-app.options("/", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://clinic-web-client-mootazmwahab-dev.apps.sandbox-m3.1530.p1.openshiftapps.com");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.sendStatus(204);
-});
 
 const doctorRoutes = require('./src/routes/doctors');
 const patientRoutes = require('./src/routes/patients');
