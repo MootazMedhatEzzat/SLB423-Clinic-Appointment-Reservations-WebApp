b7430d335209e811f99e71faa97a5e2acfa4f9e8
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-12-17 13:40:12 +0200

    Update server.js

--- clinicWebApp/server/server.js
+++ clinicWebApp/server/server.js
@@ -21,7 +21,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Handle preflight requests
-app.options('Access-Control-Allow-Origin', cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 //app.use(cors());
 
