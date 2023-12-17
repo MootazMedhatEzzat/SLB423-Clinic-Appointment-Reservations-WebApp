82ee32e62324221f3b2e29f31f6bdd947f3824f0
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-12-17 12:54:08 +0200

    Update server.js

--- clinicWebApp/server/server.js
+++ clinicWebApp/server/server.js
@@ -27,8 +27,8 @@ app.listen(port, () => {
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const https = require('https');
-const fs = require('fs');
+//const https = require('https');
+//const fs = require('fs');
 
 const app = express();
 
@@ -36,9 +36,9 @@ const defaultPort = 3000;
 const port = process.env.BE_PORT || process.argv[2] || defaultPort;
 
 // Load SSL certificate and private key
-const privateKey = fs.readFileSync('/path/to/private-key.pem', 'utf8');
-const certificate = fs.readFileSync('/path/to/certificate.pem', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
+//const privateKey = fs.readFileSync('/path/to/private-key.pem', 'utf8');
+//const certificate = fs.readFileSync('/path/to/certificate.pem', 'utf8');
+//const credentials = { key: privateKey, cert: certificate };
 
 app.use(bodyParser.json());
 
@@ -59,7 +59,7 @@ app.use('/api', patientRoutes);
 app.use('/api', authRoutes);
 
 // Create an HTTPS server
-const httpsServer = https.createServer(credentials, app);
+//const httpsServer = https.createServer(credentials, app);
 
 httpsServer.listen(port, () => {
   console.log(`Backend server is running on port ${port}`);
