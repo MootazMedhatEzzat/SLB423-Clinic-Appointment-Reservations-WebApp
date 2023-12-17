f067040171433ee14a7ab10bd759108b32b0c9ea
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-12-17 13:44:54 +0200

    Update server.js

--- clinicWebApp/server/server.js
+++ clinicWebApp/server/server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 // Handle preflight requests
 //app.options('*', cors(corsOptions));
 
-//app.use(cors());
+app.use(cors());
 
 const doctorRoutes = require('./src/routes/doctors');
 const patientRoutes = require('./src/routes/patients');
