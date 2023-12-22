cb2dc519fd6fb9574749b324b9392535b91d22f9
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-12-22 18:21:16 +0200

    Update server.js

--- clinicWebApp/server/server.js
+++ clinicWebApp/server/server.js
@@ -1,5 +1,5 @@
 // server.js
-
+/*
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -10,6 +10,33 @@ const port = process.env.BE_PORT || process.argv[2] || defaultPort;
 
 app.use(bodyParser.json());
 app.use(cors());
+
+const doctorRoutes = require('./src/routes/doctors');
+const patientRoutes = require('./src/routes/patients');
+const authRoutes = require('./src/routes/auth');
+
+app.use('/api', doctorRoutes);
+app.use('/api', patientRoutes);
+app.use('/api', authRoutes);
+
+app.listen(port, () => {
+  console.log(`Backend server is running on port ${port}`);
+});
+*/
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+const defaultPort = 3000;
+const port = process.env.BE_PORT || process.argv[2] || defaultPort;
+
+app.use(bodyParser.json());
+app.use(cors({
+  origin: '*', // Set to your React app's domain if possible
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Enable credentials (cookies, authorization headers, etc.)
+}));
 
 const doctorRoutes = require('./src/routes/doctors');
 const patientRoutes = require('./src/routes/patients');
