0ed548a5163315998068bca68ec3129ac6956a02
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-12-22 18:45:25 +0200

    Update server.js

--- clinicWebApp/server/server.js
+++ clinicWebApp/server/server.js
@@ -1,5 +1,5 @@
 // server.js
-/*
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -15,44 +15,6 @@ const doctorRoutes = require('./src/routes/doctors');
 const patientRoutes = require('./src/routes/patients');
 const authRoutes = require('./src/routes/auth');
 
-app.use('/api', doctorRoutes);
-app.use('/api', patientRoutes);
-app.use('/api', authRoutes);
-
-app.listen(port, () => {
-  console.log(`Backend server is running on port ${port}`);
-});
-*/
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const app = express();
-
-const defaultPort = 3000;
-const port = process.env.BE_PORT || process.argv[2] || defaultPort;
-
-
-app.use(bodyParser.json());
-app.use(cors());
-/*
-app.use(cors({
-  origin: 'https://clinic-web-client-mootazmwahab-dev.apps.sandbox-m3.1530.p1.openshiftapps.com', // Set to your React app's domain if possible
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-}));
-*/
-const doctorRoutes = require('./src/routes/doctors');
-const patientRoutes = require('./src/routes/patients');
-const authRoutes = require('./src/routes/auth');
-/*
-// Set CORS headers explicitly for each route
-app.use('/api', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-*/
 app.use('/api', doctorRoutes);
 app.use('/api', patientRoutes);
 app.use('/api', authRoutes);
