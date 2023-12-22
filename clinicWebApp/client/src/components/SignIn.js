ceb1ced746666053b1e9b0d86b87c2faee014480
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-12-22 19:21:58 +0200

    Update SignIn.js

--- clinicWebApp/client/src/components/SignIn.js
+++ clinicWebApp/client/src/components/SignIn.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 import '../css/SignIn.css';
 
 // Determine the backend URL based on the environment
-const backendUrl = process.env.REACT_APP_BACKEND_URL;
-
+const defaultBackendUrl = 'http://localhost:3000';
+const backendUrl = process.env.REACT_APP_BACKEND_URL || defaultBackendUrl;
 console.log(`Backend URL ${backendUrl}`);
 
 const SignIn = () => {
