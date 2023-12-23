9eb81b16a2bde4a932a78b3bc56ba0d568bafd3c
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-12-23 19:58:49 +0200

    Update database.js

--- clinicWebApp/server/database.js
+++ clinicWebApp/server/database.js
@@ -2,20 +2,30 @@
 
 const { Pool } = require('pg');
 
-const defaultDbPort = 5432;
+const defaultDbUser = 'postgres';
 const defaultDbHost = 'clinic-web-database-container';
-const dbPort = process.env.DB_PORT || defaultDbPort;
+const defaultDbName = 'clinic_database';
+const defaultDbPassword = 'clinic_database_pass';
+const defaultDbPort = 5432;
+
+const dbUser = process.env.DB_USER || defaultDbUser;
 const dbHost = process.env.DB_HOST || defaultDbHost;
+const dbName = process.env.DB_NAME || defaultDbName;
+const dbPassword = process.env.DB_PASSWORD || defaultDbPassword;
+const dbPort = process.env.DB_PORT || defaultDbPort;
 
 const pool = new Pool({
-    user: 'postgres',
+    user: dbUser,
     host: dbHost,
-    database: 'clinic_database',
-    password: 'clinic_database_pass',
+    database: dbName,
+    password: dbPassword,
     port: dbPort,
 });
 
 console.log(`Database server is running on port ${dbPort}`);
 console.log(`Database is running on host ${dbHost}`);
+console.log(`Database name ${dbName}`);
+console.log(`Database user ${dbUser}`);
+console.log(`Database password ${dbPassword}`);
 
 module.exports = pool;
