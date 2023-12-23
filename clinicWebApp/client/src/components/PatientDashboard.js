eeb0459a76c7fcecf71d8ffbffe76a6ff5e97a97
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-12-23 20:12:55 +0200

    Update PatientDashboard.js

--- clinicWebApp/client/src/components/PatientDashboard.js
+++ clinicWebApp/client/src/components/PatientDashboard.js
@@ -18,7 +18,7 @@ const PatientDashboard = () => {
 
   const fetchReservations = async () => {
     try {
-      const response = await fetch(`http://localhost:3000/api/patients/${userId}/reservations`);
+      const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/patients/${userId}/reservations`);
       if (!response.ok) {
         throw new Error('Failed To Fetch Patients Slot');
       }
@@ -36,7 +36,7 @@ const PatientDashboard = () => {
 
   const handleBooking = async () => {
     try {
-      const response = await fetch('http://localhost:3000/api/patients/bookappointment', {
+      const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/patients/bookappointment`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -58,7 +58,7 @@ const PatientDashboard = () => {
 
   const handleUpdate = async () => {
     try {
-      const response = await fetch('http://localhost:3000/api/patients/updateappointment', {
+      const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/patients/updateappointment`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
@@ -79,7 +79,7 @@ const PatientDashboard = () => {
 
   const handleCancel = async () => {
     try {
-      const response = await fetch('http://localhost:3000/api/patients/cancelappointment', {
+      const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/patients/cancelappointment`, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
@@ -102,7 +102,7 @@ const PatientDashboard = () => {
     setDoctorId(selectedDoctorId);
     if (selectedDoctorId) {
       try {
-        const response = await fetch(`http://localhost:3000/api/patients/${selectedDoctorId}/getslots`);
+        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/patients/${selectedDoctorId}/getslots`);
         if (!response.ok) {
           throw new Error('Failed to fetch doctor slots');
         }
