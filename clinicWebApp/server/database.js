02471929ab477de8c74d6e58bd7840c92044ddff
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-12-23 19:48:33 +0200

    Update database.js

--- clinicWebApp/server/database.js
+++ clinicWebApp/server/database.js
@@ -2,12 +2,20 @@
 
 const { Pool } = require('pg');
 
+const defaultDbPort = 5432;
+const defaultDbHost = 'clinic-web-database-container';
+const dbPort = process.env.DB_PORT || defaultDbPort;
+const dbHost = process.env.DB_HOST || defaultDbHost;
+
 const pool = new Pool({
     user: 'postgres',
-    host: 'clinic-web-database-container',
+    host: dbHost,
     database: 'clinic_database',
     password: 'clinic_database_pass',
-    port: 5432,
+    port: dbPort,
 });
+
+console.log(`Database server is running on port ${dbPort}`);
+console.log(`Database is running on host ${dbHost}`);
 
 module.exports = pool;
